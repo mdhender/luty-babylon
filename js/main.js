9d63a9fd1bf096cff6ba6c385450d4f31982962e
Michael Henderson <mdhender@mdhender.com>
2023-02-08 22:09:39 -0700

    added hard-coded sector list

--- js/main.js
+++ js/main.js
@@ -23,7 +23,65 @@
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-const createScene = function () {
+const fetchSector = function (x, y, z) {
+    // systems in the current sector
+    const sector = {
+        // radius: 0, // initially unknown
+        // minX:0, maxX:0, minY:0, maxY:0, minZ:0, maxZ:0,
+        origin: {x: x, y: y, z: z},
+        systems: [
+            {x: 20, y: -4, z: 28, kind: "Yellow Main Sequence"},
+            {x: 18, y: -3, z: 17, kind: "Dense Dust Cloud"},
+            {x: 18, y: -2, z: 17, kind: "Medium Dust Cloud"},
+            {x: 19, y: 7, z: -3, kind: "Blue Super Giant"},
+            {x: 10, y: -5, z: 0, kind: "Blue Super Giant"},
+            {x: -18, y: 7, z: -2, kind: "Yellow Main Sequence"},
+            {x: 13, y: 7, z: 17, kind: "Medium Dust Cloud"},
+            {x: 8, y: 11, z: -23, kind: "Dense Dust Cloud"},
+            {x: 21, y: 10, z: 7, kind: "Yellow Main Sequence"},
+            {x: 19, y: -14, z: -5, kind: "Yellow Main Sequence"},
+            {x: -5, y: 9, z: 24, kind: "Blue Super Giant"},
+            {x: -3, y: 17, z: -21, kind: "Light Dust Cloud"},
+            {x: 20, y: 0, z: -7, kind: "Yellow Main Sequence"},
+            {x: 8, y: 22, z: -18, kind: "Yellow Main Sequence"},
+            {x: -25, y: -23, z: 5, kind: "Blue Super Giant"},
+            {x: -24, y: 1, z: 23, kind: "Blue Super Giant"},
+            {x: 1, y: -22, z: 11, kind: "Dense Dust Cloud"},
+            {x: 20, y: -23, z: -2, kind: "Light Dust Cloud"}]
+    };
+
+    // determine the radius of the sector
+    sector.systems.forEach((system, ndx) => {
+        if (ndx === 0) {
+            sector.minX = system.x
+            sector.maxX = system.x
+            sector.minY = system.y
+            sector.maxY = system.y
+            sector.minZ = system.z
+            sector.maxZ = system.z
+        }
+        if (system.x < sector.minX) {
+            sector.minX = system.x
+        } else if (sector.maxX < system.x) {
+            sector.maxX = system.x
+        }
+        if (system.y < sector.minY) {
+            sector.minY = system.y
+        } else if (sector.maxY < system.y) {
+            sector.maxY = system.y
+        }
+        if (system.z < sector.minZ) {
+            sector.minZ = system.z
+        } else if (sector.maxZ < system.z) {
+            sector.maxZ = system.z
+        }
+    });
+    sector.radius = Math.max(sector.maxX - sector.minX, sector.maxY - sector.minY, sector.maxZ - sector.minZ) / 2 + 1;
+
+    return sector;
+}
+
+const createScene = function (sector) {
     // This creates a basic Babylon Scene object (non-mesh)
     const scene = new BABYLON.Scene(engine);
 
@@ -51,10 +109,19 @@ const createScene = function () {
     // Our built-in 'ground' shape.
     const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
+    // add the stars and dust clouds to the scene
+    sector.systems.forEach((system, ndx) => {
+        const mesh = BABYLON.MeshBuilder.CreateSphere("obj-${ndx}", {diameter: 1, segments: 32}, scene);
+        mesh.position.x = system.x;
+        mesh.position.y = system.y;
+        mesh.position.z = system.z;
+    });
+
     return scene;
 };
 
-const scene = createScene(); //Call the createScene function
+// Call the createScene function
+const scene = createScene(fetchSector(0, 0, 0));
 
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
